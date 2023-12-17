28e5d287d85040139181d76e9161e398e9fb93d7
Nikita Safonov <148264012+nxplosive@users.noreply.github.com>
2023-12-17 15:39:29 +0300

    Add files via upload

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+from selene import browser
+import pytest
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+   browser.config.base_url = 'https://demoqa.com'
+   browser.config.timeout = 5
+   browser.config.window_width = 1920
+   browser.config.window_height = 1080
+
+   yield
+
+   browser.quit()
