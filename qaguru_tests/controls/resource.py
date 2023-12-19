a9fae8ca1f3f2b70aa9ca5392afa19d7e11e93f2
Nikita Safonov <148264012+nxplosive@users.noreply.github.com>
2023-12-19 13:45:50 +0300

    Update resource.py

--- qaguru_tests/controls/resource.py
+++ qaguru_tests/controls/resource.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 
 def path(image_name):
-    return str(Path(__file__).parent.joinpath(f'picture/{image_name}'))
+    return str(Path(__file__).parent.parent.joinpath(f'picture/{image_name}'))
